3307cbef06613b696cbe73096721c73ffbf3cc0d
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2025-11-12 19:08:50 +0700

    Menambahkan Template Laravel Breeze sesuai tempalte instruksi: - Tampilan Login - Tampilan Register - Fungsi Login Added - Fungsi Register Added - Add Middleware Admin (sesuai module 2 pdf)
    Note: untuk register sudah bisa tetapi masih belum bisa mengakses /dashboard karena middleware hanya untuk admin sesuai module 2 pdf

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ]
+        );
+    }
+}
